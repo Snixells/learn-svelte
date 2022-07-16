28638af801cca6d76a717612d9f32265940368d7
Sebastian Schäfer <bschr9uwb@googlemail.com>
2022-07-16 18:05:58 +0200

    Componenten Structure

---
+++ src/Modal.svelte
@@ -0,0 +1,19 @@
+<script>
+    let showModal = false;
+
+    const toggleModal = () => {
+        showModal = !showModal;
+    };
+</script>
+
+<button on:click={toggleModal}>Toggle Modal</button>
+{#if showModal === true}
+    <div class="backdrop">
+        <div class="modal">
+            <p>Sign up for offers</p>
+        </div>
+    </div>
+{/if}
+
+<style>
+</style>
