6da99f96c468ec5573e64f0701c991bc98d0bfcd
Sebastian Schäfer <sebastianschafer@MBPvonSebastian.fritz.box>
2022-07-16 16:08:40 +0200

    Adding netninja tut code at #5 because it seems like a good idea

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+	let firstName = 'Peter';
+	let lastName = 'Rodjinski'
+	let city = 'Orlando';
+
+	$: fullName = `${firstName} ${lastName}`
+</script>
+
+<main>
+	<p>{fullName} from {city}</p>
+	<input type="text" bind:value={firstName}>
+	<input type="text" bind:value={lastName}>
+	<input type="text" bind:value={city}>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
