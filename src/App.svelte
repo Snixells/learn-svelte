870364f119d8b95caa71639375a6881923447bb3
Sebastian Schäfer <bschr9uwb@googlemail.com>
2022-07-16 17:06:02 +0200

    Cleanup

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,7 @@
 <script>
-	let firstName = 'Peter';
-	let lastName = 'Rodjinski'
-	let city = 'Orlando';
-
-	$: fullName = `${firstName} ${lastName}`
-	$: {
-		console.log(city);
-		console.log(fullName);
-	}
 </script>
 
 <main>
-	<p>{fullName} from {city}!</p>
-	<input type="text" bind:value={firstName}>
-	<input type="text" bind:value={lastName}>
-	<input type="text" bind:value={city}>
 </main>
 
 <style>
@@ -23,13 +10,6 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
 	}
 
 	@media (min-width: 640px) {
