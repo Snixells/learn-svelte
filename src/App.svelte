d924a113c20e521029af990426186a7f05416391
Sebastian Schäfer <bschr9uwb@googlemail.com>
2022-07-17 10:38:21 +0200

    Starting fresh project for polls site

--- src/App.svelte
+++ src/App.svelte
@@ -1,46 +1,10 @@
 <script>
-	import Modal from "./Modal.svelte";
-	import AddPersonForm from "./AddPersonForm.svelte";
-
-	let showModal = false;
-
-	let people = [
-		{ name: "yoshi", beltColour: "black", age: 25, id: 1 },
-		{ name: "mario", beltColour: "orange", age: 45, id: 2 },
-		{ name: "luigi", beltColour: "brown", age: 35, id: 3 },
-	];
-
-	const removePerson = (id) => {
-		people = people.filter((person) => person.id != id);
-	};
-
-	const addPerson = (e) => {
-		const person = e.detail;
-		people = [person, ...people];
-		showModal = false;
-	};
-	const toggleModal = () => {
-		showModal = !showModal;
-	};
+	export let name;
 </script>
 
-<Modal isPromo={false} {showModal} on:click={toggleModal}>
-	<AddPersonForm on:addPerson={addPerson} />
-</Modal>
 <main>
-	<button on:click={toggleModal}>Toggle Modal</button>
-	{#each people as person (person.id)}
-		<div>
-			<h4>{person.name}</h4>
-			{#if person.beltColour === "black"}
-				<p><strong>NINJAA</strong></p>
-			{/if}
-			<p>{person.age} {person.beltColour}</p>
-			<button on:click={() => removePerson(person.id)}>Delete</button>
-		</div>
-	{:else}
-		<p>There are no people to show</p>
-	{/each}
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
 <style>
@@ -49,6 +13,13 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
 	}
 
 	@media (min-width: 640px) {
