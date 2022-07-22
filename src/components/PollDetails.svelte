56de3261dfb5bd16b1f7bf3bdad4a26ff0ccab2e
Sebastian Schäfer <bschr9uwb@googlemail.com>
2022-07-22 11:17:53 +0200

    Adding Poll Bar Styling and Progress

--- src/components/PollDetails.svelte
+++ src/components/PollDetails.svelte
@@ -8,6 +8,8 @@
 
     // Reactive Values
     $: totalVotes = poll.votesA + poll.votesB;
+    $: percentA = Math.floor((100 / totalVotes) * poll.votesA);
+    $: percentB = Math.floor((100 / totalVotes) * poll.votesB);
 
     // Handling Votes
     const handleVote = (option, id) => {
@@ -20,11 +22,11 @@
         <h3>{poll.question}</h3>
         <p>Total votes: {totalVotes}</p>
         <div class="answer" on:click={() => handleVote("a", poll.id)}>
-            <div class="perent percent-a" />
+            <div class="percent percent-a" style="width: {percentA}%" />
             <span>{poll.answerA} ({poll.votesA})</span>
         </div>
         <div class="answer" on:click={() => handleVote("b", poll.id)}>
-            <div class="perent percent-v" />
+            <div class="percent percent-b" style="width: {percentB}%" />
             <span>{poll.answerB} ({poll.votesB})</span>
         </div>
     </div>
@@ -53,5 +55,21 @@
     span {
         display: inline-block;
         padding: 10px 20px;
+    }
+
+    .percent {
+        height: 100%;
+        position: absolute;
+        box-sizing: border-box;
+    }
+
+    .percent-a {
+        border-left: 4px solid #d91b42;
+        background: rgba(217, 27, 66, 0.2);
+    }
+
+    .percent-b {
+        border-left: 4px solid #45c496;
+        background: rgba(69, 196, 150, 0.2);
     }
 </style>
