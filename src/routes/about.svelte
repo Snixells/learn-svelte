b05dac984e123375d3b359e0f85690784ede7aea
Sebastian Schäfer <bschr9uwb@googlemail.com>
2022-07-23 16:53:09 +0200

    Adding basic routing

---
+++ src/routes/about.svelte
@@ -0,0 +1,22 @@
+<div class="about">
+    <h2>About us</h2>
+    <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum, enim
+        suscipit praesentium asperiores, inventore aspernatur officia impedit
+        neque aperiam voluptatem nobis? Quas magnam magni, accusantium expedita
+        reiciendis dolor porro laboriosam? Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Ullam ducimus error obcaecati, suscipit
+        blanditiis eius in eaque autem sit excepturi quaerat! Praesentium
+        explicabo necessitatibus voluptatem quam commodi ipsum magnam vitae.
+    </p>
+    <a href="/guides">View Guides</a>
+    <a href="/">Home</a>
+</div>
+
+<style>
+    .about {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script>
+</script>
+
+<div class="index">
+    <h2>Welcome</h2>
+    <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta
+        aspernatur commodi, temporibus quidem totam odit praesentium in aliquid!
+        Repellat excepturi molestiae iusto laboriosam nihil sint reiciendis
+        repudiandae corporis deserunt eveniet. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Error saepe soluta nostrum. Reiciendis quo
+        quasi neque deleniti laudantium molestiae non, incidunt illum natus,
+        aliquid assumenda quos blanditiis. Totam, id rerum.
+    </p>
+    <a href="/guides">View Guides</a>
+    <a href="/about">About</a>
+</div>
+
+<style>
+    .index {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
