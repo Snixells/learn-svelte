088190bbc284cf003f9d855f7c55b4c463b961ad
Sebastian Schäfer <bschr9uwb@googlemail.com>
2022-07-23 14:55:23 +0200

    Adding tweens/animtaions

--- src/components/PollDetails.svelte
+++ src/components/PollDetails.svelte
@@ -2,12 +2,16 @@
     import PollStore from "../stores/PollStore";
     import Card from "../shared/Card.svelte";
     import Button from "../shared/Button.svelte";
+    import { tweened } from "svelte/motion";
+    import { cubicOut } from "svelte/easing";
+
     export let poll = [];
 
     // Reactive Values
     $: totalVotes = poll.votesA + poll.votesB;
-    $: percentA = Math.floor((100 / totalVotes) * poll.votesA);
-    $: percentB = Math.floor((100 / totalVotes) * poll.votesB);
+    // || => If not defined (division by 0) it will be 0 => local or
+    $: percentA = Math.floor((100 / totalVotes) * poll.votesA) || 0;
+    $: percentB = Math.floor((100 / totalVotes) * poll.votesB) || 0;
 
     // Handling Votes
     const handleVote = (option, id) => {
@@ -25,6 +29,19 @@
         });
     };
 
+    // tweened percentages
+    const tweenedA = tweened(0, {
+        duration: 400,
+        easing: cubicOut,
+    });
+    const tweenedB = tweened(0, {
+        duration: 400,
+        easing: cubicOut,
+    });
+    $: tweenedA.set(percentA);
+    $: tweenedB.set(percentB);
+    $: console.log($tweenedA);
+
     // Deleting Poles
     const handleDelete = (id) => {
         PollStore.update((currentPolls) => {
@@ -38,11 +55,11 @@
         <h3>{poll.question}</h3>
         <p>Total votes: {totalVotes}</p>
         <div class="answer" on:click={() => handleVote("a", poll.id)}>
-            <div class="percent percent-a" style="width: {percentA}%" />
+            <div class="percent percent-a" style="width: {$tweenedA}%" />
             <span>{poll.answerA} ({poll.votesA})</span>
         </div>
         <div class="answer" on:click={() => handleVote("b", poll.id)}>
-            <div class="percent percent-b" style="width: {percentB}%" />
+            <div class="percent percent-b" style="width: {$tweenedB}%" />
             <span>{poll.answerB} ({poll.votesB})</span>
         </div>
     </div>
